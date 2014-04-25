990b2710328d548423449aeb9ec8d8bdd431b034
Dan Kirkwood <dangogh@gmail.com>
2014-04-25 15:28:27 -0600

    loads yaml

--- gooey.go
+++ gooey.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	"fmt"
+        //"encoding/json"
+        "fmt"
+        "gopkg.in/yaml.v1"
+        "io/ioutil"
+        "log"
+        "os"
 )
 
+func getData(dataFile string) (interface{}, error) {
+        f, err := os.Open(dataFile)
+        if err != nil {
+                log.Println(err)
+                return nil, err
+        }
+        data, err := ioutil.ReadAll(f)
+        if err != nil {
+                log.Println(err)
+                return nil, err
+        }
+        var y interface{}
+        if err := yaml.Unmarshal([]byte(data), &y); err != nil {
+                log.Println(err)
+                return nil, err
+        }
+        d, err := yaml.Marshal(y)
+        if err != nil {
+                log.Println(err)
+                return nil, err
+        }
+        return y, nil
+}
+
 func main() {
-	fmt.Println("Gooey...")
+        for _, a := range os.Args[1:] {
+                y, err := getData(a)
+                if err != nil {
+                        log.Println(err)
+                        continue
+                }
+                fmt.Print(y)
+        }
 }
