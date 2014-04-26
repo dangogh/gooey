474154e320006d77450996325aba18252fc84060
Dan Kirkwood <dangogh@gmail.com>
2014-04-26 11:13:51 -0600

    stuck on map

--- gooey.go
+++ gooey.go
@@ -28,9 +28,65 @@ func getData(dataFile string) (map[string]interface{}, error) {
         return y, nil
 }
 
+type Comet struct {
+        Name    string
+        Size    uint
+}
 type Galaxy struct {
         Name    string
         Size    uint
+}
+
+func buildObj(name, objtype string, val map[string]interface{}) (interface{}, error) {
+        fmt.Println("Creating a ", objtype)
+        var obj interface{}
+        switch objtype {
+        case "Galaxy":
+                g := Galaxy{Name: name}
+                obj = g
+        case "Comet":
+                c := Comet{Name: name}
+                obj = c
+        default:
+                return nil, fmt.Errorf("Can't create a ", objtype)
+        }
+        return obj, nil
+}
+
+func build(name string, val interface{}) (interface{}, error) {
+        switch val.(type) {
+        case string:
+                fmt.Println("type is string", name, val)
+                return val, nil
+
+        case map[string]interface{}:
+                fmt.Println("type is map", name, val)
+                vmap := val.(map[string]interface{})
+                if t, ok := vmap["Type"]; ok {
+                        fmt.Println("Creating a ", t)
+                        if objtype, ok := t.(string); ok {
+                                return buildObj(name, objtype, vmap)
+                        }
+                }
+
+                var res map[string]interface{}
+                for k, v := range vmap {
+                        r, err := build(k, v)
+                        if err != nil {
+                                return nil, err
+                        }
+                        res[k] = r
+                }
+                return res, nil
+
+        case []string:
+                fmt.Println("type is []string", name, val)
+                return val, nil
+
+        default:
+                fmt.Println("type is ??", name, val)
+                return nil, fmt.Errorf("Don't know how to create a %v", val)
+        }
 }
 
 func main() {
@@ -40,17 +96,12 @@ func main() {
                         log.Println(err)
                         continue
                 }
-                var objs []interface{}
-                var obj interface{}
-                for _, intf := range y {
-                        switch intf {
-                        case "Galaxy":
-                                obj = Galaxy{}
-                        default:
-                                log.Printf("Don't know how to create a %v\n", intf)
-                        }
-                        objs = append(objs, obj)
+                obj, err := build("", y)
+                if err != nil {
+                        fmt.Println(err)
+                        break
                 }
-                fmt.Println(objs)
+
+                fmt.Println(obj)
         }
 }
